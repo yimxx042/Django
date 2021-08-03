d5cb5e4ddad353f0e6f00cab822ac7162f91c24d
yimxx042 <79169462+yimxx042@users.noreply.github.com>
2021-08-03 11:00:42 -0500

    add date function

--- foods/views.py
+++ foods/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 
 # Create your views here.
 def index(request):
-    return render(request, 'foods/index.html')
+    today = datetime.today().date()  
+    context = {"date":today}
+    return render(request, 'foods/index.html', context=context)
