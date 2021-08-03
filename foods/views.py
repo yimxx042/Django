9cd3a569a45fb07de92851a0f3c7bf7e1bce3c1d
yimxx042 <79169462+yimxx042@users.noreply.github.com>
2021-08-03 12:59:10 -0500

    modeling database, create menu

--- foods/views.py
+++ foods/views.py
@@ -16,4 +16,6 @@ def food_detail(request, food):  #food paramenter is ~/foods/menu/food < this pa
         context['description'] = 'hot k-pop style checken!'
         context['price'] = 10
         context['img_path'] = 'foods/images/chicken.jpg'
+    else:
+        raise Http404("no food!") #raise is force to error
     return render(request, 'foods/detail.html', context=context)
--- menus/urls.py
+++ menus/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('index/', views.index),
     path('food/<str:menu>/', views.detail)
 ]
